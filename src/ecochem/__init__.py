57645ca9a86eb0983592e5c9f3789e4fecdc06d2
ClauVitt <claudia.vittorangeli@epfl.ch>
2026-05-24 18:53:09 +0200

    Import added to __init__.py

--- src/ecochem/__init__.py
+++ src/ecochem/__init__.py
@@ -1,1 +1,11 @@
+from __future__ import annotations
+
+from ecochem.DataClasses import Reaction
+from ecochem.DataClasses import Chemical
+from ecochem.DataClasses import ChemswithMass
+from ecochem.DataClasses import Solvent
+from ecochem.DataClasses import Extractant
+from ecochem.DataClasses import LiquidChemical
+
+
 __version__ = "0.1.0"
